2273e94807d93dc9e87c2654ee356e1f0d926ea9
Hydeelin12 <116590948+Hydeelin12@users.noreply.github.com>
2022-11-15 20:29:20 -0600

    estilo
    cambie el estilo de create

--- TrabajoPDF/resources/views/articulo/create.blade.php
+++ TrabajoPDF/resources/views/articulo/create.blade.php
@@ -5,6 +5,7 @@
 @endsection
 
 @section('content')
+<body style="background-color:#25BEEB;">
     <section class="content container-fluid">
         <div class="row">
             <div class="col-md-12">
@@ -27,4 +28,5 @@
             </div>
         </div>
     </section>
+</body>
 @endsection
