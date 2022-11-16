e71fecce9fa855bc25bcb56cc9e82cd2ed147671
Hydeelin12 <116590948+Hydeelin12@users.noreply.github.com>
2022-11-15 20:12:30 -0600

    estilos
    ponemos algunos estilos en pdf

--- TrabajoPDF/resources/views/articulo/pdf.blade.php
+++ TrabajoPDF/resources/views/articulo/pdf.blade.php
@@ -1,12 +1,17 @@
+<body style="background-color:#C2CFCA ;">
 <!DOCTYPE html>
 <html lang="en">
+    
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="{{ public_path('css/app.css') }}" rel="stylesheet" type=""text/css>
+
 </head>
+
+
 <body>
 <h2>Lista de articulos</h2>
 
@@ -32,3 +37,4 @@
                             </table>
 </body>
 </html>
+</body>
